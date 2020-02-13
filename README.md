4f9733348b762ed6cc57d532ef4ff1ba7c967b24
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-02-13 15:48:20 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+=Cmake / OpenMP example project=
 
+Just testing the cmake setting to have openmp enabled on linux and mac (using `brew install libomp`).
+
+Just:
+
+```
+mkdir build
+cd build
+cmake ..
+make
+./testOpenMP
+```
+
+and make sure that all your cores are running.
---
+++ testOpenMP.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+  const int size = 256256;
+  double sinTable[size];
+  
+#pragma omp parallel for schedule(dynamic)
+  for(int n=0; n<size; ++n)
+  {
+    auto d=0.0;
+    for(auto i = 0 ; i < 10000;++i)
+      d+=  std::sin(2 * M_PI * n / size);
+    sinTable[n] = d;
+  }
+  
+  double sum=0.0;
+  for(auto v: sinTable)
+    sum += v;
+  std::cout<<" Sum = "<<sum<<std::endl;
+  return 0;
+}
